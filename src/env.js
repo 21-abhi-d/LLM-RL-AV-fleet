b1f23dae2fb6f6af87d21546d1b12ebf71f5a729
Name <name@example.com>
2025-11-13 17:09:47 +1100

    aaa

--- src/env.js
+++ src/env.js
@@ -1,7 +1,23 @@
 import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
 
 export const env = createEnv({
-  server: {},
+  server: {
+    AUTH_SECRET: z.string().optional(),
+    AUTH_DISCORD_ID: z.string().optional(),
+    AUTH_DISCORD_SECRET: z.string().optional(),
+    DATABASE_URL: z.string().url().optional(),
+
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+  },
   client: {},
-  runtimeEnv: {},
+  runtimeEnv: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
+    AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+  },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  emptyStringAsUndefined: true,
 });
